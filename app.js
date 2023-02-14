1889f06806c854fdd6cb6fb532b0b8047e9c2c23
codedeporter <nwasporter70@gmail.com>
2023-02-14 07:18:21 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+let menuEl = document.querySelector('.menu')
+let listEl = document.querySelector('.list')
+
+menuEl.addEventListener('click', () => {
+    if(listEl.style.display=="none"){
+        listEl.style.display="block"
+    }else{
+        listEl.style.display="none"
+    }
+})
+
+let notes = document.querySelector(".notes")
+let note = document.querySelector(".note")
+
+function newNotes(){
+    let newNote = notes.value
+    let date = new Date
+    note.innerHTML = `<div>
+    <h1>${newNote}</h1>
+    <p>${date.toDateString()}, ${date.
+        toLocaleTimeString()}</p>
+    <div>`
+    
+}
+
